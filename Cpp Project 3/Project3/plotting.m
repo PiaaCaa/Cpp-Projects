7d17e4c15fc6780ee0d92c6291fc9e81119a2866
Jonas Fey <jonasfey98@gmail.com>
2022-11-26 14:05:07 +0100

    new pdf + m file

--- Cpp Project 3/Project3/plotting.m
+++ Cpp Project 3/Project3/plotting.m
@@ -5,15 +5,6 @@ file2 = fopen("curve_out.bin");
 B=fread(file2,[2,400],'double');
 
 
-f =@ (x) (0.5 * 1 ./ (1+exp(-3*(x+6))));
-
-
-g =@ (x) (0.5 * 1 ./ (1+exp(3*x)));
-
-x = -10:0.01:-3;
-xx = -3:0.01:5;
-
-
 figure(1);
 plot(B(1,:), B(2,:), "-r", "LineWidth",2);
 hold on;
@@ -21,6 +12,5 @@ scatter(A(1,:), A(2,:),15,"blue","filled");
 grid on;
 xlabel("x");
 ylabel("y");
-title("50x20 Grid")
-%plot(A(1,:), A(2,:), '.')
-%plot(B(1,:), B(2,:), "-r", x, f(x), "-b", xx, g(xx),"-g");
+title("50x20 Grid");
+
